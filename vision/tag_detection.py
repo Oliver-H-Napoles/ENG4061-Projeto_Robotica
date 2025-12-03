efc02e6671dfe84522e5eb854b3933a1741c5fe9
Raspberry Repo <mr.pedrocunha@gmail.com>
2025-12-03 14:20:35 -0300

    Cria script de detecção de AprilTag

---
+++ vision/tag_detection.py
@@ -0,0 +1,62 @@
+import cv2
+import apriltag
+
+def main():
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+    if not cam.isOpened():
+        print("ERRO! Não foi possível abrir a captura de vídeo.")
+        return
+
+    options = apriltag.DetectorOptions(
+        families='tag36h11',
+        refine_pose=True,
+        debug=True
+    )
+    detector = apriltag.Detector(options)
+    
+    print("Aperte 'q' para sair.")
+
+    while True:
+        ret, frame = cam.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        results = detector.detect(gray)
+
+        for r in results:
+            # extract the bounding box (x, y)-coordinates for the AprilTag
+            # and convert each of the (x, y)-coordinate pairs to integers
+            (ptA, ptB, ptC, ptD) = r.corners
+            ptB = (int(ptB[0]), int(ptB[1]))
+            ptC = (int(ptC[0]), int(ptC[1]))
+            ptD = (int(ptD[0]), int(ptD[1]))
+            ptA = (int(ptA[0]), int(ptA[1]))
+
+            # draw the bounding box of the AprilTag detection
+            cv2.line(frame, ptA, ptB, (0, 255, 0), 2)
+            cv2.line(frame, ptB, ptC, (0, 255, 0), 2)
+            cv2.line(frame, ptC, ptD, (0, 255, 0), 2)
+            cv2.line(frame, ptD, ptA, (0, 255, 0), 2)
+
+            # draw the center (x, y)-coordinates of the AprilTag
+            (cX, cY) = (int(r.center[0]), int(r.center[1]))
+            cv2.circle(frame, (cX, cY), 5, (0, 0, 255), -1)
+
+            # draw the tag family on the image
+            tagFamily = r.tag_family.decode("utf-8")
+            cv2.putText(frame, tagFamily, (ptA[0], ptA[1] - 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            print("[INFO] tag family: {}".format(tagFamily))
+        
+        cv2.imshow('APRILTAGS', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
